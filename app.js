5b2e56e96288fa63da8dc4da838a24a716152f3a
pave42 <pave420@gmail.com>
2022-03-28 17:48:35 -0300

    changes to use Heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(process.env, 3000, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
 
